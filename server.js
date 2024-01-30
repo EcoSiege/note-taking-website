d191b1d8fd8d094321faf7c63d5c098379ae53ef
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-01-30 10:56:59 -0600

    added some routes to the server file.

--- server.js
+++ server.js
@@ -3,19 +3,25 @@ const path = require('path');
 const noteData = require('./db/db.json')
 const PORT = 3001;
 
-const app = express();
+const app = process.env.PORT  ||  express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
 
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(noteData);
+});
 
-
-
-
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} post note request recieved`);
+});
 
 
 app.listen(PORT, () => {
